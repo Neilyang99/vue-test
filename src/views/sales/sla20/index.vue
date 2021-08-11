55a20fb4243c4035b76598cdd9dc9de88ca7e5e3
Neil <ycaptain@gmail.com>
2021-08-11 23:14:33 +0800

    sla20

---
+++ src/views/sales/sla20/index.vue
@@ -0,0 +1,355 @@
+<template>
+  <div class="app-container">
+    <div class="block">
+      <el-row  :gutter="20">
+        <el-col :span="6">
+          <el-input v-model="listQuery.sla10006" size="mini" placeholder="請輸入客戶姓名"></el-input>
+        </el-col>
+        <el-col :span="6">
+          <el-button type="success" size="mini" icon="el-icon-search" @click.native="search">{{ $t('button.search') }}</el-button>
+          <el-button type="primary" size="mini" icon="el-icon-refresh" @click.native="reset">{{ $t('button.reset') }}</el-button>
+        </el-col>
+      </el-row>
+      <br>
+      <el-row>
+        <el-col :span="24">
+          <el-button type="success" size="mini" icon="el-icon-plus" @click.native="add">{{ $t('button.add') }}</el-button>
+          <el-button type="primary" size="mini" icon="el-icon-edit" @click.native="edit">{{ $t('button.edit') }}</el-button>
+          <el-button type="danger" size="mini" icon="el-icon-delete" @click.native="remove">{{ $t('button.delete') }}</el-button>
+        </el-col>
+      </el-row>
+    </div>
+
+
+    <el-table :data="list" v-loading="listLoading" element-loading-text="Loading" border fit highlight-current-row
+    @current-change="handleCurrentChange">
+      <el-table-column label="銷售案號">
+        <template slot-scope="scope">
+          {{scope.row.sla20039}}
+        </template>
+      </el-table-column>
+      <el-table-column label="客戶姓名">
+        <template slot-scope="scope">
+          {{scope.row.sla20008}}
+        </template>
+      </el-table-column>
+      <el-table-column label="行動電話">
+        <template slot-scope="scope">
+          {{scope.row.sla20016}}
+        </template>
+      </el-table-column>
+      <el-table-column label="訂單日期">
+        <template slot-scope="scope">
+          {{scope.row.sla20004}}
+        </template>
+      </el-table-column>
+      <el-table-column label="負責業務員">
+        <template slot-scope="scope">
+          {{scope.row.sla10015}}
+        </template>
+      </el-table-column>
+      <el-table-column label="訂單狀態" >
+        <template slot-scope="scope">
+          {{scope.row.sla20005}}
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-dialog
+      :title="formTitle"
+      :visible.sync="formVisible"
+      width="70%">
+      <el-form ref="form" :model="form" :rules="rules" label-width="100px">
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="銷售案號"  >
+                <el-select  v-model="form.building" filterable placeholder="請選擇">
+                  <el-option
+                    v-for="item in buildingList"
+                    :key="item.id"
+                    :label="item.sla00003"
+                    :value="item.sla00002">
+                  </el-option>
+                </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="來人/來電" prop="sla10004">
+              <el-select  v-model="form.sla10004" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10004List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="性别" prop="sla10007">
+              <el-radio-group v-model="form.sla10007">
+                <el-radio label="M">男</el-radio>
+                <el-radio label="F">女</el-radio>
+              </el-radio-group>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>  
+          <el-col :span="8">
+            <el-form-item label="業務員" prop="sla10015">
+              <el-input v-model="form.sla10015"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="填表日期" prop="sla10013">
+              <el-date-picker type="date" value-format="yyyyMMdd" placeholder="選擇日期" v-model="form.sla10013" style="width: 100%;"></el-date-picker>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="客戶姓名" prop="sla10006">
+              <el-input v-model="form.sla10006"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>  
+          <el-col :span="8">
+            <el-form-item label="Email" prop="sla10012">
+              <el-input v-model="form.sla10012" ></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="電話" prop="sla10009">
+              <el-input v-model="form.sla10009" ></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="行動電話" prop="sla10010">
+              <el-input v-model="form.sla10010" ></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>  
+          <el-col :span="8">
+            <el-form-item label="連絡地址" prop="sla10011">
+              <el-input v-model="form.sla10011" ></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="區域" prop="sla10016">
+              <el-select  v-model="form.sla10016" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10016List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="需求產品" prop="sla10019">
+              <el-select  v-model="form.sla10019" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10019List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>  
+          <el-col :span="8">
+            <el-form-item label="年齡區間" prop="sla10017">
+              <el-select  v-model="form.sla10017" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10017List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="職業" prop="sla10018">
+              <el-select  v-model="form.sla10018" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10018List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="需求價位" prop="sla10027">
+              <el-select  v-model="form.sla10027" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10027List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>  
+          <el-col :span="8">
+            <el-form-item label="購買用途" prop="sla10020">
+              <el-select  v-model="form.sla10020" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10020List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="現住狀況" prop="sla10021">
+              <el-select  v-model="form.sla10021" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10021List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="車位需求" prop="sla10022">
+              <el-select  v-model="form.sla10022" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10022List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>  
+          <el-col :span="8">
+            <el-form-item label="媒體分析" prop="sla10023">
+              <el-select  v-model="form.sla10023" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10023List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="詢問重點" prop="sla10024">
+              <el-select  v-model="form.sla10024" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10024List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="需求房數" prop="sla10025">
+              <el-select  v-model="form.sla10025" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10025List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>  
+          <el-col :span="8">
+            <el-form-item label="已購原因" prop="sla10029">
+              <el-select  v-model="form.sla10029" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10029List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="洽詢戶別" prop="sla10028">
+              <el-input v-model="form.sla10028" ></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="需求坪數" prop="sla10026">
+              <el-select  v-model="form.sla10026" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10026List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>  
+          <el-col :span="8">
+            <el-form-item label="未購原因" prop="sla10030">
+              <el-select  v-model="form.sla10030" placeholder="請選擇">
+                  <el-option
+                    v-for="item in sla10030List"
+                    :key="item.key"
+                    :label="item.value"
+                    :value="item.key">
+                  </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="16">
+            <el-form-item label="備註" >
+              <el-input v-model="form.sla10033" type="textarea"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-form-item>
+          <el-button type="primary" @click="save">{{ $t('button.submit') }}</el-button>
+          <el-button @click.native="formVisible = false">{{ $t('button.cancel') }}</el-button>
+        </el-form-item>
+
+      </el-form>
+    </el-dialog>
+    <el-pagination
+                background
+                layout="total, sizes, prev, pager, next, jumper"
+                :page-sizes="[10, 20, 50, 100]"
+                :page-size="listQuery.limit"
+                :total="total"
+                :current-page.sync="listQuery.page"
+                @size-change="changeSize"
+                @current-change="fetchPage"
+                @prev-click="fetchPrev"
+                @next-click="fetchNext">
+    </el-pagination>
+  </div>
+</template>
+
+<script src="./sla20.js"></script>
+
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "src/styles/common.scss";
+</style>
+
---
+++ src/views/sales/sla20/sla20.js
@@ -0,0 +1,315 @@
+import { remove, getList, save, 
+  getSla10004,getSla10016,getSla10017,getSla10018,getSla10019,getSla10020,getSla10021,getSla10022,getSla10023,getSla10024,
+  getSla10025,getSla10026,getSla10027,getSla10029,getSla10030 } from '@/api/sales/visitor'
+import { selectList } from '@/api/sales/sla00'
+
+export default {
+  data() {
+    return {
+      formVisible: false,
+      formTitle: '新增來人/來電洽詢',
+      isAdd: true,
+      form: {
+        id: '',
+        sla10002: '',
+        sla10003: '',
+        sla10004: '',
+        sla10006: '',
+        sla10007: '',
+        sla10008: '',
+        sla10009: '',
+        sla10010: '',
+        sla10011: '',
+        sla10012: '',
+        sla10013: '',
+        sla10015: '',
+        sla10016: '',
+        sla10017: '',
+        sla10018: '',
+        sla10019: '',
+        sla10020: '',
+        sla10021: '',
+        sla10022: '',
+        sla10023: '',
+        sla10024: '',
+        sla10025: '',
+        sla10026: '',
+        sla10027: '',
+        sla10028: '',
+        sla10029: '',
+        sla10030: '',
+        sla10033: '',
+        building: '',
+        buildingName: ''
+      },
+      buildingList:[],
+      sla10004List:[],
+      sla10016List:[],
+      sla10017List:[],
+      sla10018List:[],
+      sla10019List:[],
+      sla10020List:[],
+      sla10021List:[],
+      sla10022List:[],
+      sla10023List:[],
+      sla10024List:[],
+      sla10025List:[],
+      sla10026List:[],
+      sla10027List:[],
+      sla10029List:[],
+      sla10030List:[],
+      rules: {
+        sla10004: [
+          { required: true, message: '請輸入來人或來電', trigger: 'blur' }
+        ],
+        sla10013: [
+          { required: true, message: '請輸入填表日期', trigger: 'blur' }
+        ],
+        sla10006: [
+          { required: true, message: '請輸入客戶名', trigger: 'blur' }
+        ],
+        sla10010: [
+          { required: true, message: '行動電話需要輸入', trigger: 'blur' }
+        ]
+
+      },
+      listQuery: {
+        page: 1,
+        limit:10,
+        sla10006: undefined
+      },
+      total: 0,
+      list: null,
+      listLoading: true,
+      selRow: {}
+    }
+  },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  created() {
+    this.init()
+  },
+  methods: {
+    init() {
+      this.fetchData()
+      this.fetchOptionList()
+    },
+    fetchData() {
+      this.listLoading = true
+      getList(this.listQuery).then(response => {
+        this.list = response.data.records
+        this.listLoading = false
+        this.total = response.data.total
+      })
+    },
+    fetchBuilding() {
+      selectList().then(response => {
+        this.buildingList = response.data
+      })
+    },
+    fetchOptionList(){
+      getSla10004().then(response => {this.sla10004List = response.data})
+      getSla10016().then(response => {this.sla10016List = response.data})
+      getSla10017().then(response => {this.sla10017List = response.data})
+      getSla10018().then(response => {this.sla10018List = response.data})
+      getSla10019().then(response => {this.sla10019List = response.data})
+      getSla10020().then(response => {this.sla10020List = response.data})
+      getSla10021().then(response => {this.sla10021List = response.data})
+      getSla10022().then(response => {this.sla10022List = response.data})
+      getSla10023().then(response => {this.sla10023List = response.data})
+      getSla10024().then(response => {this.sla10024List = response.data})
+      getSla10025().then(response => {this.sla10025List = response.data})
+      getSla10026().then(response => {this.sla10026List = response.data})
+      getSla10027().then(response => {this.sla10027List = response.data})
+      getSla10029().then(response => {this.sla10029List = response.data})
+      getSla10030().then(response => {this.sla10030List = response.data})
+    },
+    search() {
+      this.listQuery.page = 1
+      this.fetchData()
+    },
+    fetchNext() {
+      this.listQuery.page = this.listQuery.page + 1
+      this.fetchData()
+    },
+    fetchPrev() {
+      this.listQuery.page = this.listQuery.page - 1
+      this.fetchData()
+    },
+    fetchPage(page) {
+      this.listQuery.page = page
+      this.fetchData()
+    },
+    changeSize(limit) {
+      this.listQuery.limit = limit
+      this.fetchData()
+    },
+    reset() {
+      this.listQuery.sla10006 = ''
+      this.listQuery.page = 1
+      this.fetchData()
+    },
+    handleFilter() {
+      this.listQuery.page = 1
+      this.getList()
+    },
+    handleClose() {
+
+    },
+    handleCurrentChange(currentRow, oldCurrentRow) {
+      this.selRow = currentRow
+    },
+    resetForm() {
+      this.form = {
+        id: '',
+        sla10002: '',
+        sla10003: '',
+        sla10004: '',
+        sla10006: '',
+        sla10007: '',
+        sla10008: '',
+        sla10009: '',
+        sla10010: '',
+        sla10011: '',
+        sla10012: '',
+        sla10013: '',
+        sla10015: '',
+        sla10016: '',
+        sla10017: '',
+        sla10018: '',
+        sla10019: '',
+        sla10020: '',
+        sla10021: '',
+        sla10022: '',
+        sla10023: '',
+        sla10024: '',
+        sla10025: '',
+        sla10026: '',
+        sla10027: '',
+        sla10028: '',
+        sla10029: '',
+        sla10030: '',
+        sla10033: '',
+        building: '',
+        buildingName: ''
+      }
+    },
+    add() {
+      this.resetForm()
+      this.fetchBuilding()
+      
+      this.formTitle = '新增來人/來電洽詢表'
+      this.formVisible = true
+      this.isAdd = true
+    },
+    save() {
+      var self = this
+      this.$refs['form'].validate((valid) => {
+        this.getSelectLable()
+
+        if (valid) {
+          save({
+            id: self.form.id,
+            sla10002: self.form.building,
+            sla10003: self.form.buildingName,
+            sla10004: self.form.sla10004,
+            sla10006: self.form.sla10006,
+            sla10007: self.form.sla10007,
+            sla10008: self.form.sla10008,
+            sla10009: self.form.sla10009,
+            sla10010: self.form.sla10010,
+            sla10011: self.form.sla10011,
+            sla10012: self.form.sla10012,
+            sla10013: self.form.sla10013,
+            sla10015: self.form.sla10015,
+            sla10016: self.form.sla10016,
+            sla10017: self.form.sla10017,
+            sla10018: self.form.sla10018,
+            sla10019: self.form.sla10019,
+            sla10020: self.form.sla10020,
+            sla10021: self.form.sla10021,
+            sla10022: self.form.sla10022,
+            sla10023: self.form.sla10023,
+            sla10024: self.form.sla10024,
+            sla10025: self.form.sla10025,
+            sla10026: self.form.sla10026,
+            sla10027: self.form.sla10027,
+            sla10028: self.form.sla10028,
+            sla10029: self.form.sla10029,
+            sla10030: self.form.sla10030,
+            sla10033: self.form.sla10033
+          }).then(response => {
+            console.log(response)
+            this.$message({
+              message: '提交成功',
+              type: 'success'
+            })
+            this.fetchData()
+            this.formVisible = false
+          })
+        } else {
+          return false
+        }
+      })
+    },
+    checkSel() {
+      if (this.selRow && this.selRow.id) {
+        return true
+      }
+      this.$message({
+        message: '請挑選其中一筆資料....',
+        type: 'warning'
+      })
+      return false
+    },
+    viewLog(visitorId) {
+      this.$router.push({ path: '/visitorLog', query: { visitorId: visitorId }})
+    },
+    edit() {
+      if (this.checkSel()) {
+        this.fetchBuilding()
+        this.isAdd = false
+        this.form = this.selRow
+        //this.form.id = this.selRow.sla10001
+        this.form.building = this.selRow.sla10002
+        this.formTitle = '修改洽詢表'
+        this.formVisible = true
+      }
+    },
+    getSelectLable(){
+      this.buildingList.forEach(item =>{
+        if(item.sla00002 === this.form.building){
+          this.form.buildingName = item.sla00003;
+        }
+      })
+    },
+    remove() {
+      if (this.checkSel()) {
+        var id = this.selRow.id
+        this.$confirm('確定刪除資料?', '提示', {
+          confirmButtonText: '確定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          remove(id).then(response => {
+            this.$message({
+              message: '操作成功',
+              type: 'success'
+            })
+            this.fetchData()
+          })
+        }).catch(() => {
+        })
+      }
+    }
+
+  }
+}
